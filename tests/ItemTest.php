1479a163010e38285fe4c94d2afdaeb2f7357741
hugoresende <hugo.resende@iservices.pt>
2024-05-06 19:56:12 +0100

    testing Reflexion classes

---
+++ tests/ItemTest.php
@@ -0,0 +1,49 @@
+<?php
+use App\Item;
+use PHPUnit\Framework\TestCase;
+
+
+
+class ItemTest extends TestCase
+{
+
+    public function testDescriptionIsNotEmpty()
+    {
+
+        $item = new App\Item;
+        $this->assertNotEmpty($item->getDescription());
+
+    }
+
+    /**
+     * [testIdIsAnInteger]
+     * works with protected methods but not private ones
+     * @return [type]
+     * 
+     */
+    public function testIdIsAnInteger()
+    {
+        $item = new App\ItemChild;
+        $this->assertIsInt($item->getID());
+    }
+
+
+    /**
+     * [testTokenIsAString]
+     * to test private methods we can use ReflectionClass
+     * https://www.php.net/manual/en/class.reflectionclass.php
+     * @return [type]
+     */
+    public function testTokenIsAString()
+    {
+        $item = new App\ItemChild;
+        $reflector = new ReflectionClass(Item::class);
+        $method = $reflector->getMethod('getToken');
+        $method->setAccessible(true);
+        $result = $method->invoke($item);
+        $this->assertIsString($result);
+    }
+
+    
+
+}
